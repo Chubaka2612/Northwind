250601252e724f3027818698ce78af1edff52faf
Viktoriia Skirko <Viktoriia_Skirko@epam.com>
2024-11-08 14:58:14 +0200

    [task3] Implement task

---
+++ Northwind.Web/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Northwind.Web.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper, int imageId, string linkText)
+        {
+            var url = $"/Image/{imageId}";
+            var tagBuilder = new TagBuilder("a");
+            tagBuilder.Attributes["href"] = url;
+            tagBuilder.InnerHtml.Append(linkText);
+
+            return tagBuilder;
+        }
+    }
+}
---
+++ Northwind.Web/Models/BreadcrumbViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+public class Breadcrumb
+{
+    public string Label { get; }
+    public string Action { get; }
+    public string Controller { get; }
+    public object RouteId { get; }
+
+    public Breadcrumb(string label, string action, string controller, object routeId = null)
+    {
+        Label = label;
+        Action = action;
+        Controller = controller;
+        RouteId = routeId;
+    }
+}
+
+public class BreadcrumbViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(ViewContext viewContext)
+    {
+        var routeValues = viewContext?.RouteData?.Values;
+
+        string controller = routeValues["controller"]?.ToString();
+        string action = routeValues["action"]?.ToString();
+        object id = routeValues.ContainsKey("id") ? routeValues["id"] : null;
+        string title = viewContext.ViewData["Title"]?.ToString();
+
+        var breadcrumbs = new List<Breadcrumb>
+        {
+            new Breadcrumb("Home", "Index", "Home")
+        };
+
+        if (!string.IsNullOrEmpty(controller))
+        {
+            if (!string.Equals(action, "Index", StringComparison.OrdinalIgnoreCase))
+            {
+                breadcrumbs.Add(new Breadcrumb(controller, "Index", controller));
+            }
+
+            breadcrumbs.Add(new Breadcrumb(title ?? action, action, controller, id));
+        }
+
+        return View("BreadcrumbViewComponent", breadcrumbs);
+    }
+}
